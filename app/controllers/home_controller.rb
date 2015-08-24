21f87cbdad8774c0c2a7fd7d3e620be101538e36
dmitry <kosenkodmitryv@gmail.com>
2015-08-24 16:58:48 +0300

    mockup with left slide menu. navigation between topics, stories. created a home controller.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+  # def details
+  #
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'home#index'
+
+
+  get 'stories' => 'home#stories', as: :story_list
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
