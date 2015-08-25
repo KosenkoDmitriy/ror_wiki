6aeb5be7bf53df1999f11cebcb89bbe56fc943d1
dmitry <kosenkodmitryv@gmail.com>
2015-08-25 15:15:52 +0300

    Added translations. Implemented the navigation of the Topics Controller. Updated the topic views and partials.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,18 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.order(updated_at: :desc, title: :asc)
+    @topic = @topics.try(:first)
+    @stories = @topic.try(:stories)
   end
+
+  def show
+    @topics = Topic.order(updated_at: :desc, title: :asc)
+
+    id = params[:id] if params[:id].present?
+    @topic = Topic.find(id) if Topic.exists?(id)
+    @stories = @topic.try(:stories)
+  end
+
+  private
+
 end
