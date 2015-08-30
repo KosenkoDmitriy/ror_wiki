d635677e36313af92ee7028c0c4a0db41895b84d
dmitry <kosenkodmitryv@gmail.com>
2015-08-30 22:59:24 +0300

    Backend. Multiple fields. Add new topics and stories to the story.

--- app/admin/story.rb
+++ app/admin/story.rb
@@ -1,12 +1,32 @@
 ActiveAdmin.register Story do
   menu priority: 2, label: "Stories"
-  # sidebar "Story Details", only: [:show, :edit] do
-  #   ul do
-  #     li link_to "Sources",    admin_story_sources_path(story)
-  #   end
-  # end
+  sidebar "Story Details", only: [:show, :edit] do
+    ul do
+      li link_to "Sources", admin_story_sources_path(story)
+      ul do
+        if story.sources.present? && story.sources.any?
+          story.sources.each do |source|
+            li link_to "#{source.title} (#{source.created_at.strftime("%d/%m/%y %H:%M")})", admin_story_source_path(story, source)
+          end
+        else
+        li "Source hasn't been added to the story: "+story.title
+        end
+      end
+      li link_to "Topics", admin_topics_path()
+      ul do
+        if story.topics.present? && story.topics.any?
+          story.topics.each do |topic|
+            li link_to "#{topic.title} (#{topic.created_at.strftime("%d/%m/%y %H:%M")})", admin_topic_path(topic)
+          end
+        else
+          li "Topic hasn't been added to the story: "+story.title
+        end
+      end
+    end
+  end
 
-  permit_params :title, :text, :stext, :is_approved, topic_ids: [], source_ids: []
+
+  permit_params :title, :text, :stext, :is_approved, topic_ids: [], source_ids: [], topics_attributes: [:id, :title, :stext, :text, :is_approved, :_destroy], sources_attributes: [:id, :title, :url, :_destroy]
 
   controller do
     # This code is evaluated within the controller class
@@ -40,7 +60,7 @@ ActiveAdmin.register Story do
 
     private
     def story_params
-      params.require(:story).permit(:title, :text, :stext, :is_approved, topic_ids: [], source_ids: []) #, topics_attributes: [], sources_attributes: [])
+      params.require(:story).permit(:title, :text, :stext, :is_approved, topic_ids: [], source_ids: [], topics_attributes: [:id, :title, :stext, :text, :is_approved, :_destroy], sources_attributes: [:id, :title, :url, :_destroy])
     end
   end
 
@@ -57,7 +77,7 @@ ActiveAdmin.register Story do
   end
 
   form do |f|
-    f.inputs "Story Details" do
+    f.inputs "Story: "+story.title do
       f.input :title
       f.input :stext
       f.input :text
@@ -65,21 +85,36 @@ ActiveAdmin.register Story do
     end
 
     f.inputs 'Sources.' do
-      f.input :source_ids, as: :select, include_blank: true, multiple: true, selected: f.object.sources.map { |s| s.id }, collection: Source.all.map { |i| ["#{i.title} (#{i.created_at})", i.id] } #, options_for_select(["Page", "Organization", "Promotion"], p.object)
-      # f.has_many :sources, new_record: true do |p|
-      #   #p.input :title
-      #   # or maybe even
-      #   p.input :id, label: 'Source', as: :select, multiple: false, selected: p.object.id, collection: Source.all.map { |i| ["#{i.title}:#{i.url}",i.id] }
-      #   p.actions
-      # end
+      f.inputs 'Existing Sources.' do
+        f.input :source_ids, as: :select, include_blank: true, multiple: true, selected: f.object.sources.map { |s| s.id }, collection: Source.all.map { |i| ["#{i.title} (#{i.created_at})", i.id] }
+      end
+
+      f.inputs 'New Sources.' do
+        f.has_many :sources, allow_destroy: true, new_record: true do |p|
+          p.input :title
+          p.input :url, as: :url
+          #p.input :id, label: 'Source', as: :select, multiple: false, selected: p.object.id, collection: Source.all.map { |i| ["#{i.title}:#{i.url}",i.id] }
+          p.actions
+        end
+      end
     end
 
     f.inputs 'Topics.' do
-      f.input :topic_ids, as: :select, include_blank: true, multiple: true, selected: f.object.topics.map { |t| t.id }, collection: Topic.all.map { |i| ["#{i.title} (#{i.created_at})", i.id] } #, options_for_select(["Page", "Organization", "Promotion"], p.object)
-      # f.has_many :topics, new_record: true do |p|
-      #   p.input :id, label: 'Topic', as: :select, multiple: false, selected: p.object.id, collection: Topic.all.map { |i| [i.title, i.id] } #collection: Topic.all.map { |i| "#{i.title} (#{i.created_at})" } #, options_for_select(["Page", "Organization", "Promotion"], p.object)
-      #   p.actions
-      # end
+      f.inputs 'Existing Topics.' do
+        f.input :topic_ids, as: :select, include_blank: true, multiple: true, selected: f.object.topics.map { |t| t.id }, collection: Topic.all.map { |i| ["#{i.title} (#{i.created_at})", i.id] }
+      end
+
+      f.inputs 'New Topics.' do
+        # f.input :topic_ids, as: :select, include_blank: true, multiple: true, selected: f.object.topics.map { |t| t.id }, collection: Topic.all.map { |i| ["#{i.title} (#{i.created_at})", i.id] } #, options_for_select(["Page", "Organization", "Promotion"], p.object)
+        f.has_many :topics, allow_destroy: true, new_record: true do |p|
+          # p.input :id, label: 'Topic', as: :select, multiple: false, selected: p.object.id, collection: Topic.all.map { |i| [i.title, i.id] } #collection: Topic.all.map { |i| "#{i.title} (#{i.created_at})" } #, options_for_select(["Page", "Organization", "Promotion"], p.object)
+          p.input :title
+          p.input :stext
+          p.input :text
+          p.input :is_approved, as: :radio
+          p.actions
+        end
+      end
     end
 
     # f.inputs 'Topics.' do
@@ -90,7 +125,7 @@ ActiveAdmin.register Story do
   end
 
 end
-#
-# ActiveAdmin.register Source do
-#   belongs_to :story
-# end
+
+ActiveAdmin.register Source do
+  belongs_to :story
+end
