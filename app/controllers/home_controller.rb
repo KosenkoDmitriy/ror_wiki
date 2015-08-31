45bd31eaa2cf668c20c40b80e48287365241d09b
dmitry <kosenkodmitryv@gmail.com>
2015-09-01 01:00:40 +0300

    1000 stories on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   def index
     # page = params[:page] if params[:page].present?
     page = params[:page] || 1
-    @stories = Story.order(date_time: :desc, title: :asc).try(:page, page)
+    per_page = params[:per] || 1000
+    @stories = Story.order(date_time: :desc, title: :asc).try(:page, page).try(:per,per_page)
   end
 
   # def details
