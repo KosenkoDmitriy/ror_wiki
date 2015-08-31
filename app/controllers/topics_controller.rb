2723b2f5fceca81cd406146cd76b887235fb673c
dmitry <kosenkodmitryv@gmail.com>
2015-08-31 20:35:10 +0300

    Frontend. Display only approved topics.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,14 +2,14 @@ class TopicsController < ApplicationController
 
   def index
     page = params[:page] || 1
-    @topics = Topic.order(updated_at: :desc, title: :asc).try(:page, page)
+    @topics = get_approved_topics(page)
     @topic = @topics.try(:first)
     @stories, @story = get_stories_and_story_from_topic(@topic)
   end
 
   def show
     page = params[:page] || 1
-    @topics = Topic.order(updated_at: :desc, title: :asc).try(:page, page)
+    @topics = get_approved_topics(page)
     id = params[:id] if params[:id].present?
     if Topic.exists?(id)
       @topic = Topic.find(id)
@@ -18,9 +18,14 @@ class TopicsController < ApplicationController
   end
 
   private
+  def get_approved_topics(page)
+    topics = Topic.order(updated_at: :desc, title: :asc).try(:where, is_approved: true).try(:page, page)
+    return topics
+  end
+
   def get_stories_and_story_from_topic(topic)
     count = topic.try(:stories).try(:count) || 0
-    stories = topic.try(:stories).try(:order, updated_at: :desc, created_at: :desc, title: :asc).try(:where, is_approved:true).try(:page,0).try(:per, count)
+    stories = topic.try(:stories).try(:order, updated_at: :desc, created_at: :desc, title: :asc).try(:where, is_approved: true).try(:page, 0).try(:per, count)
     story = stories.try(:first)
     return stories, story
   end
