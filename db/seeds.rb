3d6f0ec27aa45bc2e3beb57690db4d252d063d66
dmitry <kosenkodmitryv@gmail.com>
2015-08-31 23:15:38 +0300

    Fix db seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,12 +25,16 @@ def random_approved_status
   [true, false].sample
 end
 
+email = 'admin@example.com'
+password = 'password'
+User.create(email: email, password: password, password_confirmation: password) if !User.exists?(email:email)
+
 topics_count = 20
 stories_count = 20
-moderation_count = 20
+moderation_count = 2
 (1..topics_count).to_a.each do |topic_no|
 
-  topic = Topic.find_or_create_by!(title:"Topic #{topic_no}")
+  topic = Topic.find_or_create_by!(title: "Topic #{topic_no}")
   topic.date_time = random_date_time()
   topic.is_approved = random_approved_status()
   puts "-"*100
@@ -39,7 +43,7 @@ moderation_count = 20
     title = "Story # #{story_no} of the Topic # #{topic_no}"
     text = title + " description " * 59
     stext = text.truncate(50, omission: '...')
-    story = Story.find_or_create_by!(title: title, text:text, stext:stext)
+    story = Story.find_or_create_by!(title: title, text: text, stext: stext)
     story.is_approved = random_approved_status()
     story.save
     if story.present?
@@ -49,50 +53,49 @@ moderation_count = 20
         title = "Moderation Story # #{story_no} of the Topic # #{topic_no}"
         text = title + " description " * 59
         stext = text.truncate(50, omission: '...')
-        moderation = Moderation.find_or_create_by!(title: title, text:text, stext:stext)
+        moderation = Moderation.find_or_create_by!(title: title, text: text, stext: stext)
         moderation.is_approved = random_approved_status()
-        moderation.topics << topic if !story.topics.exists?(topics: {id:topic.id})
+        moderation.topics << topic if !story.topics.exists?(topics: {id: topic.id})
         puts "++mstory: #{moderation.try(:title)}"
         puts "+++mtopic: #{topic.try(:title)}"
-        sources = [{url:"https://en.wikipedia.org/wiki/Barack_Obama", title:"Wikipedia"},
-                   {url:"https://www.facebook.com/barackobama", title:"Facebook"},
-                   {url:"https://twitter.com/barackobama?lang=ru", title:"Twitter"},
-                   {url: "https://plus.google.com/+BarackObama", title:"Google+"},
-                   {url:"https://www.linkedin.com/in/barackobama", title:"Linkedin"}
+        sources = [{url: "https://en.wikipedia.org/wiki/Barack_Obama", title: "Wikipedia"},
+                   {url: "https://www.facebook.com/barackobama", title: "Facebook"},
+                   {url: "https://twitter.com/barackobama?lang=ru", title: "Twitter"},
+                   {url: "https://plus.google.com/+BarackObama", title: "Google+"},
+                   {url: "https://www.linkedin.com/in/barackobama", title: "Linkedin"}
         ]
         sources.each do |source|
           url = source[:url]
           title = source[:title]
           # source_title = "Source of the story #{story.try(:title)}"
-          source = Source.find_or_create_by!(title:title, url:url) # added sources to the story
+          source = Source.find_or_create_by!(title: title, url: url) # added sources to the story
           puts "+++msource: #{source.try(:title)} #{source.try(:url)}"
-          moderation.sources << source if !story.sources.exists?(sources: {id:source.id})
+          moderation.sources << source if !story.sources.exists?(sources: {id: source.id})
         end
 
         moderation.save
         story.moderations << moderation
       end
 
-      sources = [{url:"https://en.wikipedia.org/wiki/Barack_Obama", title:"Wikipedia"},
-                     {url:"https://www.facebook.com/barackobama", title:"Facebook"},
-                     {url:"https://twitter.com/barackobama?lang=ru", title:"Twitter"},
-                     {url: "https://plus.google.com/+BarackObama", title:"Google+"},
-                     {url:"https://www.linkedin.com/in/barackobama", title:"Linkedin"}
+      sources = [{url: "https://en.wikipedia.org/wiki/Barack_Obama", title: "Wikipedia"},
+                 {url: "https://www.facebook.com/barackobama", title: "Facebook"},
+                 {url: "https://twitter.com/barackobama?lang=ru", title: "Twitter"},
+                 {url: "https://plus.google.com/+BarackObama", title: "Google+"},
+                 {url: "https://www.linkedin.com/in/barackobama", title: "Linkedin"}
       ]
       sources.each do |source|
         url = source[:url]
         title = source[:title]
         # source_title = "Source of the story #{story.try(:title)}"
-        source = Source.find_or_create_by!(title:title, url:url) # added sources to the story
+        source = Source.find_or_create_by!(title: title, url: url) # added sources to the story
         puts "++source: #{source.try(:title)} #{source.try(:url)}"
-        story.sources << source if !story.sources.exists?(sources: {id:source.id})
+        story.sources << source if !story.sources.exists?(sources: {id: source.id})
       end
       # story.sources = story.sources.uniq #now work
       story.save
-      topic.stories << story if topic.present? && !topic.stories.exists?(stories: {id:story.id}) # added stories in the topic
+      topic.stories << story if topic.present? && !topic.stories.exists?(stories: {id: story.id}) # added stories in the topic
     end
   end
   # topic.stories = topic.stories.uniq #now work
   topic.save
-  end
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+end
