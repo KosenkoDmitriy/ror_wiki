16a3cdb56254b92acac5c1525e3943465978dc66
dmitry <kosenkodmitryv@gmail.com>
2015-09-20 19:33:01 +0300

    Fixed sources and date_time updation.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -14,63 +14,67 @@ class StoriesController < ApplicationController
   def show
     id = params[:id] if params[:id].present?
     @story = Story.find(id)
-    @topic = @story.try(:topics).try(:last)
-  end
-
-
-  def new
-    tid = params[:topic_id]
-    @topic = Topic.find(tid) if Topic.exists?(tid)
-    @story = Story.new
-    # @topic = @story.try(:topics).try(:last)
-  end
-
-
-  def create
-    tid = params[:topic_id]
-    @topic = Topic.find(tid) if Topic.exists?(tid)
-
-    if simple_captcha_valid?
-      @story = Story.new(story_params)
-      if @story.save!
-        redirect_to topic_unconfirmed_story_path(@topic, @story)
-      else
-        render new_topic_story_path(@topic)
-      end
-    else
-      #todo: display error message to frontend
-      render new_topic_story_path(@topic)
-    end
-  end
-
-
-  def edit
-    id = params[:id]
-    @story = Story.find(id) if Story.exists?(id)
 
     tid = params[:topic_id]
     @topic = Topic.find(tid) if Topic.exists?(tid)
     # @topic = @story.try(:topics).try(:last)
-
-    # redirect_to_edit
   end
 
 
-  def update
-    new_story, @topic = create_story_unconfirmed
+  # def new
+  #   tid = params[:topic_id]
+  #   @topic = Topic.find(tid) if Topic.exists?(tid)
+  #
+  #   @story = Story.new
+  #   # @topic = @story.try(:topics).try(:last)
+  # end
 
-    if simple_captcha_valid?
-      if new_story.save!
-        # redirect_to_edit new_story
-        redirect_to topic_unconfirmed_story_path @topic, new_story
-      else
-        redirect_to_edit @story
-      end
-    else
-      #todo: display error message to frontend
-      redirect_to_edit @story
-    end
-  end
+
+  # def create
+  #   tid = params[:topic_id]
+  #   @topic = Topic.find(tid) if Topic.exists?(tid)
+  #
+  #   if simple_captcha_valid?
+  #     @story = Story.new(story_params)
+  #     if @story.save!
+  #       redirect_to topic_unconfirmed_story_path(@topic, @story)
+  #     else
+  #       render new_topic_story_path(@topic)
+  #     end
+  #   else
+  #     #todo: display error message to frontend
+  #     render new_topic_story_path(@topic)
+  #   end
+  # end
+
+
+  # def edit
+  #   id = params[:id]
+  #   @story = Story.find(id) if Story.exists?(id)
+  #
+  #   tid = params[:topic_id]
+  #   @topic = Topic.find(tid) if Topic.exists?(tid)
+  #   # @topic = @story.try(:topics).try(:last)
+  #
+  #   # redirect_to_edit
+  # end
+
+
+  # def update
+  #   new_story, @topic = create_story_unconfirmed
+  #
+  #   if simple_captcha_valid?
+  #     if new_story.save!
+  #       # redirect_to_edit new_story
+  #       redirect_to topic_unconfirmed_story_path @topic, new_story
+  #     else
+  #       redirect_to_edit @story
+  #     end
+  #   else
+  #     #todo: display error message to frontend
+  #     redirect_to_edit @story
+  #   end
+  # end
 
 
   private
@@ -79,6 +83,10 @@ class StoriesController < ApplicationController
   def story_params
     #params.require(:story).permit(:title, :text, :topic_ids, :source_ids,  topic_ids:[], source_ids:[], :topic_ids=>{:id=>[]}, :source_ids=>{:id=>[]})
     params.require(:story).permit(:title, :text, :date_time, topic_ids: [], sources_attributes: [:id, :title, :url, :_destroy])
+  end
+
+  def sources_params
+    params.require(:story).permit(sources_attributes: [:id, :title, :url, :_destroy])
   end
 
   def create_story_unconfirmed
@@ -90,8 +98,8 @@ class StoriesController < ApplicationController
     @story = Story.find(id) if Story.exists?(id)
 
 
-    new_story = Story.new(story_params.except(:id, :is_approved,:sources_attributes ))
-    new_story.save!
+    # new_story = Story.new(story_params.except(:id, :is_approved,:sources_attributes ))
+    #new_story.save!
     # new_story.topics.build
     # new_story.topics = [topic]
     # story.attributes = story_orig.attributes
@@ -99,7 +107,26 @@ class StoriesController < ApplicationController
     # story.is_approved=false
     # story.save!
     #@story.save!
-    #@story.update_attributes(story.attributes.except(:id, :is_approved, :sources))
+
+    new_story = @story.dup #@story.clone for rails < 3.1
+    new_story.save
+
+    #new_story = Story.new(story_params.except(:id, :is_approved, :sources_attributes ))
+    # new_story.save!
+    # new_story.update_attributes(params[:story][:sources_attributes])
+
+    # new_story.update_attributes(story_params)
+    # new_story.update_attributes(sources_params)
+    # new_story.assign_attributes( sources_params)
+    # source_ids = params[:story][:sources_attributes]
+    # if source_ids.present? and source_ids.any?
+    #   source_ids.each do |source_id|
+    #     if Source.exists?(source)
+    #       source = Source.find(source)
+    #       new_story.sources << source if new_story.present? and new_story.sources.present? and !new_story.sources.exists?(source)
+    #     end
+    #   end
+    # end
     return new_story, topic
   end
 
--- app/controllers/unconfirmed/stories_controller.rb
+++ app/controllers/unconfirmed/stories_controller.rb
@@ -9,22 +9,56 @@ class Unconfirmed::StoriesController < ApplicationController
     @story, @topic = get_uncofirmed_story_and_topic_from_params
   end
 
+  def new
+    tid = params[:topic_id] if params[:topic_id].present?
+    @topic = Topic.find(tid) if Topic.exists?(tid)
+    @story = Story.new
+  end
+
   def update
+    @errors = []
     @story, @topic = get_uncofirmed_story_and_topic_from_params
     if simple_captcha_valid?
       # @moderation.update_attributes(story_params.except(:id,:sources_attributes))
       @story.update_attributes(story_params)
 
       if @story.save!
-        redirect_to (@story)
+        redirect_to topic_unconfirmed_story_path @topic, @story
       else
         render "edit"
       end
     else
       #todo: display error message to frontend
       # redirect_to_new_moderation
+      @errors << I18n.t("simple_captcha.message.default")
       render "edit"
-    end  end
+    end
+  end
+
+  def create
+    @story, @topic = get_uncofirmed_story_and_topic_from_params()
+
+    if simple_captcha_valid?
+      # @moderation.update_attributes(moderation_params.except(:id,:sources_attributes))
+      @story.update_attributes(story_params)
+      # dt=story_params[:date_time]
+      # dt3=params[:story][:date_time]
+      # @story.date_time = dt
+      @story.topics << @topic if !@story.topics.exists?(@topic)
+      if @story.save!
+        # redirect_to (@story)
+        # redirect_to "show"
+        # render "new"
+        redirect_to topic_unconfirmed_story_path(@topic, @story)
+
+      else
+        render "new"
+      end
+    else
+      #todo: display error message to frontend
+      render "new"
+    end
+  end
 
   private
 
@@ -34,11 +68,14 @@ class Unconfirmed::StoriesController < ApplicationController
 
   def get_uncofirmed_story_and_topic_from_params
     tid = params[:topic_id] if params[:topic_id].present?
+    topic = Topic.find(tid) if Topic.exists?(tid)
+
     id = params[:id] if params[:id].present?
     sid = params[:story_id] if params[:story_id].present?
+
     id = sid if id.blank? and sid.present?
-    story = Story.find(id)# if Story.exists?(id)
-    topic = Topic.find(tid) if Topic.exists?(tid)
+    story = Story.find(id) if Story.exists?(id)
+    story = Story.new story_params if story.blank?
     return story, topic
   end
 
