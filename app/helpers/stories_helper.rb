fcebcac338b2ff540b4b46b9ac7235babb1e50ec
dmitry <kosenkodmitryv@gmail.com>
2015-09-24 22:15:25 +0300

    fixed bug in jQuery (ajax called twice)
    Is there no in-DOM solution for establishing a target element to load the response? For example:
    
    <%= link_to "Show panda", "images/panda.gif", {remote: true, data-target: '#panda-holder' %>
    
    Then every remote link could call something generic like:
    
    $('[data-remote=true]').on('click', function (e) {
    e.preventdefault();
    $($(this).attr('data-remote')).load($(this).attr('href'));
    });
    
    Or it could be more elegantly worked into rails.js. This avoids splitting ajax behaviour into dozens of .js.erb files, and writing format.js for every method in the controller. Sure wish something like that was built into rails.js.
    
    Turbolinks is a great solution and I'm using it in a re-write, but for now I have to deal with a rails 3.1.10 app with loads of .on and e.preventdefaults in application.js

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,16 +2,15 @@ module StoriesHelper
 
   def group_by_date_time items_orig
     default_topic = Topic.find_or_create_by(title: t("topic.no_topic"), is_approved: true)
-    # date_time_m_y = items.try(:first).try(:date_time).try(:strftime, "%b %Y") || items.try(:first).try(:updated_at).try(:strftime, "%b %Y")
-    # is_parent = true
     dt_items = []
     items_orig.map { |item| dt_items << {"date_time": item.try(:date_time).try(:strftime, "%b %Y"), "items":[]} }
     dt_items.uniq!
+
     dt_items.each do |dt_item|
       items_orig.each do |item|
         item_date_time = item.try(:date_time).try(:strftime, "%b %Y") || item.try(:updated_at).try(:strftime, "%b %Y")
         topic = item.try(:topics).try(:last) || default_topic
-        item_date_time_b_y = item.try(:date_time).try(:strftime, "%b %Y") || item.try(:updated_at).try(:strftime, "%b %Y")
+        # item_date_time_b_y = item.try(:date_time).try(:strftime, "%b %Y") || item.try(:updated_at).try(:strftime, "%b %Y")
         item_date_time_d_b_h_m = item.try(:date_time).try(:strftime, "%d %B %H:%M") || item.try(:updated_at).try(:strftime, "%d %B %H:%M")
 
         if dt_item.try(:[], :date_time) == item_date_time
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   end
 
   get "ajax_all_stories" => "stories#ajax", as: :ajax_all_stories
-  get "ajax_topic_show" => "topics#ajax", as: :ajax_topic_stories
+  # get "ajax_topic_show" => "topics#ajax", as: :ajax_topic_stories
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
