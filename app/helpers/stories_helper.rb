ca0735ea7495e5ccc9c9b629c6cc9530dac1e500
dmitry <kosenkodmitryv@gmail.com>
2015-09-27 12:13:28 +0300

    Remove story time view from home  and story (except unconfirmed story) pages.

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -12,9 +12,9 @@ module StoriesHelper
         topic = item.try(:topics).try(:last) || default_topic
         # item_date_time_b_y = item.try(:date_time).try(:strftime, "%b %Y") || item.try(:updated_at).try(:strftime, "%b %Y")
         item_date_time_d_b_h_m = item.try(:date_time).try(:strftime, "%d %B %H:%M") || item.try(:updated_at).try(:strftime, "%d %B %H:%M")
-
+        item_date_time_d_b = item.try(:date_time).try(:strftime, "%d %B") || item.try(:updated_at).try(:strftime, "%d %B")
         if dt_item.try(:[], :date_time) == item_date_time
-          dt_item[:items] << {"item": item, "topic": topic, "dt": item_date_time_d_b_h_m}
+          dt_item[:items] << {"item": item, "topic": topic, "dt":item_date_time_d_b, "dt_full": item_date_time_d_b_h_m}
         end
       end
     end
@@ -34,5 +34,9 @@ module StoriesHelper
     end
 
     return result.try(:html_safe)
+  end
+
+  def date_time_without_time dt
+    return dt.try(:strftime, "%d %B")
   end
 end
