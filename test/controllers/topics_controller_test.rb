2327c72549c3b25836968f89aff9fef89a99b6a7
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-24 21:40:37 +0300

    101 / 638 LOC (15.83%) covered.

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -4,4 +4,8 @@ class TopicsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get topics_path
+    assert_response :success
+  end
 end
