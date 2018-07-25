fbefec70d27c20d6979c6a631a37283599576b76
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-25 21:05:46 +0300

    Stories Controller for Show Action.  All Files (42.96% covered at 0.59 hits/line).

--- test/controllers/unconfirmed/stories_controller_test.rb
+++ test/controllers/unconfirmed/stories_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
-class Unconfirmed::StoriesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class Unconfirmed::StoriesControllerTest < ActionDispatch::IntegrationTest #ActionController::TestCase
+  test "show" do
+    topic = topics(:one)
+    story = stories(:one)
+    sourceUrl = 'https://en.wikipedia.org/'
+    story.sources << Source.create!(url: sourceUrl)
+    get topic_unconfirmed_story_path topic, story
+    assert_equal "show", @controller.action_name
+    assert_match story.title, @response.body
+    assert_match story.text, @response.body
+    assert_match story.sources.first.url, @response.body
+    assert_match topic.title, @response.body
+  end
 end
