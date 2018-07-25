c35766b890793d059e44853818159d5a4cd6703e
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-25 10:49:56 +0300

    clearing routes, refactoring of a topics controller test

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,16 +1,10 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest # ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "should get index" do
     get topics_url
     assert_equal "index", @controller.action_name
-    # get_via_redirect topics_path
-    get topics_path
-    assert_response :redirect
-    follow_redirect!
+    get_via_redirect topics_path
     assert_response :success
     assert_select ".title-header h1", "Homepage"
     assert_select "p.text-center", "No any stories found"
@@ -20,7 +14,6 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest # ActionController:
     get topic_url(topic)
     assert_response :success
     topic_blank = Topic.new id: -1
-    # topic_blank.id = -1
     get topic_path(topic_blank)
     assert_response :missing
   end
