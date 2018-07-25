f8df9f85ff7f5f5750207f71a7f782ac7aa892c5
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-25 10:07:27 +0300

    Topics Controller (96.43 % covered). All files 27.48% covered.

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,11 +1,31 @@
 require 'test_helper'
 
-class TopicsControllerTest < ActionController::TestCase
+class TopicsControllerTest < ActionDispatch::IntegrationTest # ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
   test "should get index" do
+    get topics_url
+    assert_equal "index", @controller.action_name
+    # get_via_redirect topics_path
     get topics_path
+    assert_response :redirect
+    follow_redirect!
     assert_response :success
+    assert_select ".title-header h1", "Homepage"
+    assert_select "p.text-center", "No any stories found"
+  end
+  # test "ajax reuest" do
+  #   get '/ajax', xhr: true
+  #   assert_response :success
+  # end
+  test "should get show" do
+    topic = topics(:one)
+    get topic_url(topic)
+    assert_response :success
+    topic_blank = Topic.new id: -1
+    # topic_blank.id = -1
+    get topic_path(topic_blank)
+    assert_response :missing
   end
 end
