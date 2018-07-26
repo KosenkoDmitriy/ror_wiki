f3823fb8e1b82d5d63903d94d5c189086d5a9739
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-26 09:10:05 +0300

    Unconfirmed Stories Controller. Test for edit action. All Files (86.46% covered at 1.52 hits/line).

--- test/controllers/unconfirmed/stories_controller_test.rb
+++ test/controllers/unconfirmed/stories_controller_test.rb
@@ -1,40 +1,72 @@
 require 'test_helper'
 
 class Unconfirmed::StoriesControllerTest < ActionDispatch::IntegrationTest #ActionController::TestCase
-  test "show" do
-    topic = topics(:one)
-    story = stories(:one)
-    topic.stories << story
+  # before every single test
+  setup do
+    @topic = topics(:one)
+    @story = stories(:one)
+    @topic.stories << @story
     sourceUrl = 'https://en.wikipedia.org/'
-    story.sources << Source.create!(url: sourceUrl)
-    get topic_unconfirmed_story_path topic, story
-    assert_equal "show", @controller.action_name
-    assert_match story.title, @response.body
-    assert_match story.text, @response.body
-    assert_match story.sources.first.url, @response.body
-    assert_equal story.sources.count, 1
-    assert_equal story.topics.count, 1
-    assert_equal topic.stories.count, 1
-    assert_match topic.title, @response.body
+    @story.sources << Source.create!(url: sourceUrl)
   end
+
+  # called after every single test
+  teardown do
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+
+  test "show" do
+    get topic_unconfirmed_story_path @topic, @story
+    assert_equal "show", @controller.action_name
+    assert_match @story.title, @response.body
+    assert_match @story.text, @response.body
+    assert_match @story.sources.first.url, @response.body
+    assert_equal @story.sources.count, 1
+    assert_equal @story.topics.count, 1
+    assert_equal @topic.stories.count, 1
+    assert_match @topic.title, @response.body
+  end
+
   test "index" do
-    topic = topics(:one)
-    get topic_unconfirmed_stories_path topic
+    get topic_unconfirmed_stories_path @topic
     assert_response :redirect
     follow_redirect!
     assert_response :success
   end
+
   test "edit" do
-    topic = topics(:one)
-    story = stories(:one)
-    topic.stories << story
-    sourceUrl = 'https://en.wikipedia.org/'
-    story.sources << Source.create!(url: sourceUrl)
-    get topic_unconfirmed_story_path topic, story
+    get edit_topic_unconfirmed_story_path @topic, @story
     assert_response :success
-    assert_match story.title, @response.body
-    assert_match story.text, @response.body
-    assert_match story.sources.first.url, @response.body
-    assert_match topic.title, @response.body
+    assert_match @story.title, @response.body
+    assert_match @story.text, @response.body
+    assert_match @story.sources.first.url, @response.body
+    assert_match @topic.title, @response.body
+
+    assert_select "input[value=?]", @story.title
+    assert_select "input[type=submit]"
   end
+
+  test "should update story" do
+    patch topic_unconfirmed_story_path @topic, @story, params: { story: {title: "updated story"}}
+    assert_redirected_to topic_unconfirmed_story_path @topic, @story
+    @story.reload
+    assert_equal "updated story", @story.title
+  end
+
+  test "should not update story if invalid story's data" do #TODO handle exception 
+    patch topic_unconfirmed_story_path @topic, @story, params: { story: {title: nil, is_approved: 123}}
+    assert_redirected_to topic_unconfirmed_story_path @topic, @story
+    @story.reload
+    assert_equal false, @story.is_approved
+  end
+
+  # test "should destroy story" do
+  #   assert_difference('Story.count', -1) do
+  #     delete topic_unconfirmed_story_path @topic, @story
+  #   end
+  #
+  #   assert_redirected_to topic_unconfirmed_stories_path
+  # end
+
 end
