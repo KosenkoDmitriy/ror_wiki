7ed00e841acdbc8b8183b646effc9bb1ccd5b34e
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-26 09:08:59 +0300

    Unconfirmed Stories Controller. Tests for index and edit actions.

--- test/controllers/unconfirmed/stories_controller_test.rb
+++ test/controllers/unconfirmed/stories_controller_test.rb
@@ -4,10 +4,34 @@ class Unconfirmed::StoriesControllerTest < ActionDispatch::IntegrationTest #Acti
   test "show" do
     topic = topics(:one)
     story = stories(:one)
+    topic.stories << story
     sourceUrl = 'https://en.wikipedia.org/'
     story.sources << Source.create!(url: sourceUrl)
     get topic_unconfirmed_story_path topic, story
     assert_equal "show", @controller.action_name
+    assert_match story.title, @response.body
+    assert_match story.text, @response.body
+    assert_match story.sources.first.url, @response.body
+    assert_equal story.sources.count, 1
+    assert_equal story.topics.count, 1
+    assert_equal topic.stories.count, 1
+    assert_match topic.title, @response.body
+  end
+  test "index" do
+    topic = topics(:one)
+    get topic_unconfirmed_stories_path topic
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+  test "edit" do
+    topic = topics(:one)
+    story = stories(:one)
+    topic.stories << story
+    sourceUrl = 'https://en.wikipedia.org/'
+    story.sources << Source.create!(url: sourceUrl)
+    get topic_unconfirmed_story_path topic, story
+    assert_response :success
     assert_match story.title, @response.body
     assert_match story.text, @response.body
     assert_match story.sources.first.url, @response.body
